ac2e31ff581a567b720aa401c3d0baa6d9534e09
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-07-11 11:41:45 -0500

    Cleaned up plugin abstraction.

--- pipeline.py
+++ pipeline.py
@@ -7,43 +7,18 @@ import secrets
 from waggle.protocol.v0 import *
 
 
-def parse_version_string(s):
-    match = re.match('(\d+)\.(\d+)\.(\d+)', s)
-
-    if match is None:
-        raise ValueError('Invalid version string.')
-
-    return tuple(int(x) for x in match.groups())
-
-
 class Plugin:
 
-    def __init__(self):
+    def __init__(self, username, password):
         self.logger = logging.getLogger('pipeline.Plugin')
 
-        self.plugin_id = os.environ.get('WAGGLE_PLUGIN_ID', 0)
-        self.logger.debug('Plugin ID is %s.', self.plugin_id)
-
-        self.plugin_instance = os.environ.get('WAGGLE_PLUGIN_INSTANCE', 0)
-        self.logger.debug('Plugin instance is %s.', self.plugin_instance)
-
-        self.plugin_version = parse_version_string(os.environ.get('WAGGLE_PLUGIN_VERSION', '0.0.0'))
-        self.logger.debug('Plugin version is %s.', self.plugin_version)
-
-        self.plugin_token = os.environ.get('WAGGLE_PLUGIN_TOKEN', '')
-        self.logger.debug('Plugin token is %s.', self.plugin_token)
-
+        self.user_id = username
         self.run_id = secrets.randbelow(0xffffffff)
-        self.logger.debug('Plugin run ID is %s.', self.run_id)
-
-        # self.queue = 'in-plugin-{}-{}-{}'.format(self.plugin_id, self.plugin_version[0], 0)
-        #
-
-        self.user_id = 'plugin_{}_{}_{}'.format(self.plugin_id, self.plugin_version[0], self.plugin_instance)
+        self.queue = 'in-{}'.format(username)
 
         credentials = pika.credentials.PlainCredentials(
-            username=self.user_id,
-            password=self.plugin_token)
+            username=username,
+            password=password)
 
         parameters = pika.ConnectionParameters(
             credentials=credentials,
@@ -52,20 +27,20 @@ class Plugin:
         self.connection = pika.BlockingConnection(parameters)
         self.channel = self.connection.channel()
 
-        # self.channel.queue_declare(self.queue, durable=True)
+        self.channel.queue_declare(
+            queue=self.queue,
+            durable=True)
 
     def publish(self, body):
         self.logger.debug('Publishing body %s', body)
 
-        properties = pika.BasicProperties(
-            delivery_mode=2,
-            user_id=self.user_id)
-
         self.channel.basic_publish(
-            exchange='',
-            routing_key='data',
-            body=body,
-            properties=properties)
+            exchange='data',
+            routing_key='',
+            properties=pika.BasicProperties(
+                delivery_mode=2,
+                user_id=self.user_id),
+            body=body)
 
     def get_waiting_messages(self):
         while True:
@@ -77,7 +52,8 @@ class Plugin:
             try:
                 datagrams = unpack_datagrams(body)
             except ValueError:
-                print('invalid datagrams received')
+                self.channel.basic_ack(delivery_tag=method.delivery_tag)
+                print('Dropping invalid datagrams.')
                 continue
 
             for datagram in datagrams:
