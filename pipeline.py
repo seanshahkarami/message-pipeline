4b9ef30a5562eace7524015c25b5629e1dd2fa7b
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-07-11 11:32:55 -0500

    Working on message validator.

--- pipeline.py
+++ pipeline.py
@@ -1,40 +1,71 @@
 #!/usr/bin/env python3
+import logging
+import os
+import re
 import pika
 import secrets
 from waggle.protocol.v0 import *
 
-# WAGGLE_PLUGIN_CACERTFILE
-# WAGGLE_PLUGIN_CERTFILE
-# WAGGLE_PLUGIN_KEYFILE
+
+def parse_version_string(s):
+    match = re.match('(\d+)\.(\d+)\.(\d+)', s)
+
+    if match is None:
+        raise ValueError('Invalid version string.')
+
+    return tuple(int(x) for x in match.groups())
+
 
 class Plugin:
 
-    def __init__(self, config):
-        self.run_id = secrets.randbelow(0xffffffff)
+    def __init__(self):
+        self.logger = logging.getLogger('pipeline.Plugin')
 
-        self.config = config
-        self.queue = 'in-plugin-{}-{}-{}'.format(config['plugin_id'], config['plugin_major_version'], 0)
+        self.plugin_id = os.environ.get('WAGGLE_PLUGIN_ID', 0)
+        self.logger.debug('Plugin ID is %s.', self.plugin_id)
+
+        self.plugin_instance = os.environ.get('WAGGLE_PLUGIN_INSTANCE', 0)
+        self.logger.debug('Plugin instance is %s.', self.plugin_instance)
+
+        self.plugin_version = parse_version_string(os.environ.get('WAGGLE_PLUGIN_VERSION', '0.0.0'))
+        self.logger.debug('Plugin version is %s.', self.plugin_version)
+
+        self.plugin_token = os.environ.get('WAGGLE_PLUGIN_TOKEN', '')
+        self.logger.debug('Plugin token is %s.', self.plugin_token)
+
+        self.run_id = secrets.randbelow(0xffffffff)
+        self.logger.debug('Plugin run ID is %s.', self.run_id)
+
+        # self.queue = 'in-plugin-{}-{}-{}'.format(self.plugin_id, self.plugin_version[0], 0)
+        #
+
+        self.user_id = 'plugin_{}_{}_{}'.format(self.plugin_id, self.plugin_version[0], self.plugin_instance)
 
         credentials = pika.credentials.PlainCredentials(
-            username=config.get('username', 'admin'),
-            password=config.get('password', 'testing'))
+            username=self.user_id,
+            password=self.plugin_token)
 
         parameters = pika.ConnectionParameters(
             credentials=credentials,
-            virtual_host='beehive')
+            virtual_host='node1')
 
         self.connection = pika.BlockingConnection(parameters)
         self.channel = self.connection.channel()
 
-        self.channel.queue_declare(self.queue, durable=True)
+        # self.channel.queue_declare(self.queue, durable=True)
 
     def publish(self, body):
-        datagram = pack_datagrams([{
-            'plugin_id': self.config['plugin_id'],
-            'body': body,
-        }])
+        self.logger.debug('Publishing body %s', body)
 
-        self.channel.basic_publish('data', '', datagram)
+        properties = pika.BasicProperties(
+            delivery_mode=2,
+            user_id=self.user_id)
+
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='data',
+            body=body,
+            properties=properties)
 
     def get_waiting_messages(self):
         while True:
