1eca777be3e681192234dcfed953dea51ded6dda
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-07-11 15:21:38 -0500

    Cleanup.

---
+++ plugins/example.plugin/pipeline.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import logging
+import os
+import pika
+import secrets
+from waggle.protocol.v0 import *
+
+
+class Plugin:
+
+    def __init__(self, credentials=None):
+        self.logger = logging.getLogger('pipeline.Plugin')
+
+        if credentials is None:
+            credentials = os.environ['WAGGLE_PLUGIN_CREDENTIALS'].split(':')
+
+        username, password = credentials
+
+        self.user_id = username
+        self.run_id = secrets.randbelow(0xffffffff)
+        self.queue = 'in-{}'.format(username)
+
+        credentials = pika.credentials.PlainCredentials(
+            username=username,
+            password=password)
+
+        parameters = pika.ConnectionParameters(
+            credentials=credentials,
+            virtual_host='node1')
+
+        self.connection = pika.BlockingConnection(parameters)
+        self.channel = self.connection.channel()
+
+        self.channel.queue_declare(
+            queue=self.queue,
+            durable=True)
+
+    def publish(self, body):
+        self.logger.debug('Publishing body %s', body)
+
+        self.channel.basic_publish(
+            exchange='data',
+            routing_key='',
+            properties=pika.BasicProperties(
+                delivery_mode=2,
+                user_id=self.user_id),
+            body=body)
+
+    def get_waiting_messages(self):
+        while True:
+            method, properties, body = self.channel.basic_get(queue=self.queue)
+
+            if body is None:
+                break
+
+            try:
+                datagrams = unpack_datagrams(body)
+            except ValueError:
+                self.channel.basic_ack(delivery_tag=method.delivery_tag)
+                print('Dropping invalid datagrams.')
+                continue
+
+            for datagram in datagrams:
+                yield datagram
+
+            self.channel.basic_ack(delivery_tag=method.delivery_tag)
