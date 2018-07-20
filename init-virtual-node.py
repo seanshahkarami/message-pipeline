d651a2abdd5215a06a8301f4c5c05b370a0959fd
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-07-20 06:17:48 -0500

    added tool for setting up a virtual node environment

---
+++ init-virtual-node.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import json
+import re
+import subprocess
+
+
+def get_node_id_from_cert(certfile):
+    output = subprocess.check_output([
+        'openssl',
+        'x509',
+        '-in', certfile,
+        '-text'
+    ]).decode()
+
+    return re.search(r'CN=node([0-9a-fA-F]+)', output).group(1)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('beehive_host')
+parser.add_argument('ssl_dir')
+args = parser.parse_args()
+
+ssl_dir = os.path.abspath(args.ssl_dir)
+
+cacertfile = os.path.join(ssl_dir, 'cacert.pem')
+certfile = os.path.join(ssl_dir, 'cert.pem')
+keyfile = os.path.join(ssl_dir, 'key.pem')
+
+node_id = get_node_id_from_cert(certfile)
+receiver_id = node_id.rjust(16, '0')
+
+beehive_url = 'amqps://{}:23181?auth_mechanism=external&cacertfile={}&certfile={}&keyfile={}&verify=verify_peer'.format(
+    args.beehive_host,
+    cacertfile,
+    certfile,
+    keyfile)
+
+subprocess.check_output([
+    'rabbitmq-plugins',
+    'enable',
+    'rabbitmq_management',
+    'rabbitmq_shovel',
+    'rabbitmq_shovel_management',
+])
+
+shovel_config = json.dumps({
+    "src-uri": "amqp://localhost",
+    "src-queue": "to-beehive",
+    "dest-uri": beehive_url,
+    "dest-exchange": "messages",
+})
+
+subprocess.check_output([
+    'rabbitmqctl',
+    'set_parameter',
+    'shovel',
+    'shovel-messages-to-beehive',
+    shovel_config])
+
+
+shovel_config = json.dumps({
+    "src-uri": beehive_url,
+    "src-queue": "to-node-{}".format(receiver_id),
+    "dest-uri": "amqp://localhost",
+    "dest-queue": "messages",
+})
+
+subprocess.check_output([
+    'rabbitmqctl',
+    'set_parameter',
+    'shovel',
+    'shovel-messages-from-beehive',
+    shovel_config])
