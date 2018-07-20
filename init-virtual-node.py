dbe474b7b94491d2ad9e37fc3ee9d66c1339dd31
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-07-20 16:04:35 -0500

    Easing testing / deployment into virtual hosts.

--- init-virtual-node.py
+++ init-virtual-node.py
@@ -6,15 +6,17 @@ import re
 import subprocess
 
 
-def get_node_id_from_cert(certfile):
-    output = subprocess.check_output([
+def read_certificate_text(path):
+    return subprocess.check_output([
         'openssl',
         'x509',
         '-in', certfile,
         '-text'
     ]).decode()
 
-    return re.search(r'CN=node([0-9a-fA-F]+)', output).group(1)
+
+def get_node_id_from_cert(path):
+    return re.search(r'CN=node([0-9a-fA-F]+)', read_certificate_text(path)).group(1)
 
 
 parser = argparse.ArgumentParser()
@@ -30,6 +32,9 @@ keyfile = os.path.join(ssl_dir, 'key.pem')
 
 node_id = get_node_id_from_cert(certfile)
 receiver_id = node_id.rjust(16, '0')
+
+# should design so can be used within vhosts...
+# then, i can actually test this on my machine.
 
 beehive_url = 'amqps://{}:23181?auth_mechanism=external&cacertfile={}&certfile={}&keyfile={}&verify=verify_peer'.format(
     args.beehive_host,
@@ -58,7 +63,6 @@ subprocess.check_output([
     'shovel',
     'shovel-messages-to-beehive',
     shovel_config])
-
 
 shovel_config = json.dumps({
     "src-uri": beehive_url,
