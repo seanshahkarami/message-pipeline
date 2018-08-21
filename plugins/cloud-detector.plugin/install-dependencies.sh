3b0de3f7597949829948b11be3999e9f21f90b34
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-08-21 10:28:50 -0500

    adding dependency list

---
+++ plugins/cloud-detector.plugin/install-dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cleanup() {
+	waggle-switch-to-operation-mode
+}
+
+trap cleanup EXIT
+
+waggle-switch-to-safe-mode
+
+apt-get install \
+  build-essential \
+  cython3 \
+  python3-matplotlib \
+  python3-numpy \
+  python3-pil \
+  python3-scipy \
+  python3-skimage
