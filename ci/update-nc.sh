b2456ac1448e963578e1ff697ee026453796296e
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-09-11 10:44:03 -0500

    cleanup

--- ci/update-nc.sh
+++ ci/update-nc.sh
@@ -4,29 +4,16 @@ cleanup() {
   waggle-switch-to-operation-mode
 }
 
-unlock() {
-  if [ -z "$unlocked"]; then
-    trap cleanup EXIT
-    waggle-switch-to-safe-mode
-    unlocked="unlocked"
-  fi
-}
+waggle-switch-to-safe-mode
+trap cleanup EXIT
 
 cd /wagglerw/pywaggle
-git fetch
-
-if ! git status | grep up-to-date; then
-  git pull
-  pip3 install -U .
-fi
+git pull
+pip3 install -U .
 
 cd /wagglerw/message-pipeline
-git fetch
-
-if ! git status | grep up-to-date; then
-  git pull
-  rsync -av /wagglerw/message-pipeline/systemd/node/ /etc/systemd/system
-  rsync -av /wagglerw/message-pipeline/systemd/nc/ /etc/systemd/system
-  systemd enable $(ls /wagglerw/message-pipeline/systemd/node) \
-                 $(ls /wagglerw/message-pipeline/systemd/nc)
-fi
+git pull
+rsync -av /wagglerw/message-pipeline/systemd/node/ /etc/systemd/system
+rsync -av /wagglerw/message-pipeline/systemd/nc/ /etc/systemd/system
+systemd enable $(ls /wagglerw/message-pipeline/systemd/node) \
+               $(ls /wagglerw/message-pipeline/systemd/nc)
